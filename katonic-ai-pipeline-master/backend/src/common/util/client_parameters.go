7a06d82876c4200f989445da9fa6e68b6da2eb20
Nikhil Das <91112508+nikhil-das-katonic@users.noreply.github.com>
2022-04-22 18:03:43 +0530

    Terminated Files

--- katonic-ai-pipeline-master/backend/src/common/util/client_parameters.go
+++
@@ -1,8 +0,0 @@
-package util
-
-// ClientParameters contains parameters needed when creating a client
-type ClientParameters struct {
-	// Use float64 instead of float32 here to use flag.Float64Var() directly
-	QPS   float64
-	Burst int
-}
